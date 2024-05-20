06abc41ab3fc20e5e416afa26b6ac8a189764d1d
TheSecondBread <gamezone2904@gmail.com>
2024-05-20 17:20:50 +0530

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import Header from './Components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import ContextProvider from './Store/ContextProvider';
+
+import { Analytics } from "@vercel/analytics/react"
+
 function App() {
   return(
     <ContextProvider>
     <Header></Header>
+    <Analytics />
     </ContextProvider>
   );
 }
